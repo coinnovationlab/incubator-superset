7056d202dd2067b6ebb54339bd5fe15ac072e444
etomaselli <33021319+etomaselli@users.noreply.github.com>
2018-02-27 16:25:23 +0100

    removed consumer key and secret

--- sco_configuration/superset_config.py
+++ sco_configuration/superset_config.py
@@ -18,8 +18,8 @@ CUSTOM_SECURITY_MANAGER = AACSecurityManager
 # ----------------------------------------------------
 # Authentication config for OAuth2
 # ----------------------------------------------------
-#AUTH_TYPE = AUTH_OAUTH
-AUTH_TYPE = AUTH_DB
+AUTH_TYPE = AUTH_OAUTH
+#AUTH_TYPE = AUTH_DB #uncomment only if you need to access Superset as the default admin user
 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #avoids using https
 AAC_USER_PROFILE_ENDPOINT = 'https://localhost:8243/aacprofile/1.0.0/basicprofile/me'
@@ -27,13 +27,13 @@ AAC_USER_ROLES_ENDPOINT = 'https://localhost:8243/aacroles/1.0.0/userroles/me'
 AAC_DASHBOARD_CONTEXT = 'sco.dashboard'
 AAC_ROLE_PREFIX = 'dash_'
 TENANT_ROLE_PREFIX = 'tenant_'
-PROVIDER_ROLE = 'PROVIDER'
+PROVIDER_ROLE = 'ROLE_PROVIDER'
 
 OAUTH_PROVIDERS = [
     {'name': 'aac', 'icon': 'fa-google', 'token_key': 'access_token',
         'remote_app': {
-            'consumer_key': 'e294bd92-1189-4057-8336-49d9600477bf',
-            'consumer_secret': 'b9d8c88b-17e7-40bc-9107-59110a610753',
+            'consumer_key': '', #add your consumer key/client ID
+            'consumer_secret': '', #add your consumer secret
             'base_url': 'http://localhost:8080/aac/resources/token', #not used?
             'request_token_params': {
               'scope': 'profile.basicprofile.me user.roles.me'
